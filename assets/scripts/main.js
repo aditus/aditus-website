e4036217c43fecb2be7c9fbe6525bf5f41bf4049
DESKTOP-2668FKE\mark <mark@heart-media.com>
2017-11-30 11:09:58 +0800

    Update for token sale section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -40,11 +40,26 @@ jQuery(document).ready(function($) {
         $(this).prop('Counter',0).animate({
             Counter: $(this).text()
         }, {
-            duration: 4000,
+            duration: 8000,
             easing: 'swing',
             step: function (now) {
-                $(this).text(Math.ceil(now));
+                //$(this).text(Math.ceil());
+                $(this).text(floatval(now).toFixed(2));
             }
         });
     });
 });
+
+function floatval (mixedVar) {
+    //  discuss at: http://locutus.io/php/floatval/
+    // original by: Michael White (http://getsprink.com)
+    //      note 1: The native parseFloat() method of JavaScript returns NaN
+    //      note 1: when it encounters a string before an int or float value.
+    //   example 1: floatval('150.03_page-section')
+    //   returns 1: 150.03
+    //   example 2: floatval('page: 3')
+    //   example 2: floatval('-50 + 8')
+    //   returns 2: 0
+    //   returns 2: -50
+    return (parseFloat(mixedVar) || 0)
+}
