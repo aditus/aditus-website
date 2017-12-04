ae33edc4cfce39a888fc73b5dc48a49c451d8095
Ahmed Zulkamal <zulkamal@gmail.com>
2017-12-04 19:31:48 +0800

    combine amounts raised

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,20 +1,4 @@
-function floatval (mixedVar) {
-    //  discuss at: http://locutus.io/php/floatval/
-    // original by: Michael White (http://getsprink.com)
-    //      note 1: The native parseFloat() method of JavaScript returns NaN
-    //      note 1: when it encounters a string before an int or float value.
-    //   example 1: floatval('150.03_page-section')
-    //   returns 1: 150.03
-    //   example 2: floatval('page: 3')
-    //   example 2: floatval('-50 + 8')
-    //   returns 2: 0
-    //   returns 2: -50
-    return (parseFloat(mixedVar) || 0);
-}
-
 jQuery(document).ready(function($) {
-    $('.dropdown-toggle').dropdown();
-
   $( ".menu-div" ).click(function() {
     $( ".mobile-nav" ).toggleClass('open');
     $(".menu-div").toggleClass('open');
@@ -51,17 +35,4 @@ jQuery(document).ready(function($) {
     autoPlay: false,
     infinite: false,
   });
-
-    $('.count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 8000,
-            easing: 'swing',
-            step: function (now) {
-                //$(this).text(Math.ceil());
-                $(this).text(floatval(now).toFixed(2));
-            }
-        });
-    });
 });
